9bcc9f36f17dbee0897ccd71391ed506b4e1e395
Stéphane <eicher31@hotmail.com>
2023-01-06 12:07:29 +0100

    Add module mail_template_data

---
+++ mail_template_data/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Mail Template Data',
+    'version': '15.0.1.0',
+    'category': 'Tools',
+    'summary': 'Import template for email',
+    'description': """
+        This module import specific data for JumpTo client
+    """,
+    'author': 'JumpTo',
+    'website': 'https://jumpto.ch',
+    'depends': ['mail'],
+    'data': [
+        'data/salutaion_template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
